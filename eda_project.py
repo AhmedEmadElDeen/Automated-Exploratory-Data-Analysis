a8c4d7f716fc597d24a4d182c07accda06a02fcf
Ahmed Emad ElDeen <64612594+AhmedEmadElDeen@users.noreply.github.com>
2023-08-19 23:23:00 +0300

    Update eda_project.py
    added option to choose file path and extension through input

--- eda_project.py
+++ eda_project.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+file_path = input('Insert the path of your dataframe')
+file_format = input('What is your data format?')
+
 def load_data(file_path, file_format):
     if file_format == 'csv':
         df = pd.read_csv(file_path)
@@ -10,7 +13,7 @@ def load_data(file_path, file_format):
     return df
 
 def data_inspection(df):
-
+    
     print('Data types:\n',df.dtypes)
     print('Null data:\n',df.isnull().sum())
 
@@ -28,7 +31,7 @@ def preprocess_data(df):
     # Handle missing values
     df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
     df[categorical_columns] = df[categorical_columns].fillna('Unknown')
-
+    
     return df
 
 
@@ -54,7 +57,7 @@ def visualize_data(df, target_column):
 
 
 def main():
-    df = load_data(file_path,file_format)
+    df = load_data(file_path,file_format) 
     data_inspection(df)
     preprocessed_data = preprocess_data(df)
     target_column = input('Choose column to visualise')
